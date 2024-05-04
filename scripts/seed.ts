97e7ff7eb19e863bf44cc02c82a3e552f5b308de
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2024-05-04 18:20:01 +0530

    updated seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -66,6 +66,48 @@ const main = async () => {
         description: "Learn the basics of French",
         order: 1,
       },
+      {
+        id: 2,
+        courseId: 1,
+        title: "Unit 1",
+        description: "Learn the basics of Spanish",
+        order: 1,
+      },
+      {
+        id: 3,
+        courseId: 2,
+        title: "Unit 1",
+        description: "Learn the basics of Italian",
+        order: 1,
+      },
+      {
+        id: 4,
+        courseId: 4,
+        title: "Unit 1",
+        description: "Learn the basics of Korean",
+        order: 1,
+      },
+      {
+        id: 5,
+        courseId: 5,
+        title: "Unit 1",
+        description: "Learn the basics of German",
+        order: 1,
+      },
+      {
+        id: 6,
+        courseId: 6,
+        title: "Unit 1",
+        description: "Learn the basics of Japanese",
+        order: 1,
+      },
+      {
+        id: 7,
+        courseId: 7,
+        title: "Unit 1",
+        description: "Learn the basics of Russian",
+        order: 1,
+      },
     ]);
 
     await db.insert(schema.lessons).values([
@@ -99,6 +141,186 @@ const main = async () => {
         order: 5,
         title: "Get around town",
       },
+      {
+        id: 6,
+        unitId: 2, // Unit 2 (Learn the basics...)
+        order: 1,
+        title: "Use basic phrases",
+      },
+      {
+        id: 7,
+        unitId: 2, // Unit 2 (Learn the basics...)
+        order: 2,
+        title: "Greet people",
+      },
+      {
+        id: 8,
+        unitId: 2, // Unit 2 (Learn the basics...)
+        order: 3,
+        title: "Introduce yourself",
+      },
+      {
+        id: 9,
+        unitId: 2, // Unit 2 (Learn the basics...)
+        order: 4,
+        title: "Use the present tense",
+      },
+      {
+        id: 10,
+        unitId: 2, // Unit 2 (Learn the basics...)
+        order: 5,
+        title: "Get around town",
+      },
+      {
+        id: 11,
+        unitId: 3, // Unit 3 (Learn the basics...)
+        order: 1,
+        title: "Use basic phrases",
+      },
+      {
+        id: 12,
+        unitId: 3, // Unit 3 (Learn the basics...)
+        order: 2,
+        title: "Greet people",
+      },
+      {
+        id: 13,
+        unitId: 3, // Unit 3 (Learn the basics...)
+        order: 3,
+        title: "Introduce yourself",
+      },
+      {
+        id: 14,
+        unitId: 3, // Unit 3 (Learn the basics...)
+        order: 4,
+        title: "Use the present tense",
+      },
+      {
+        id: 15,
+        unitId: 3, // Unit 3 (Learn the basics...)
+        order: 5,
+        title: "Get around town",
+      },
+      {
+        id: 16,
+        unitId: 4, // Unit 4 (Learn the basics...)
+        order: 1,
+        title: "Use basic phrases",
+      },
+      {
+        id: 17,
+        unitId: 4, // Unit 4 (Learn the basics...)
+        order: 2,
+        title: "Greet people",
+      },
+      {
+        id: 18,
+        unitId: 4, // Unit 4 (Learn the basics...)
+        order: 3,
+        title: "Introduce yourself",
+      },
+      {
+        id: 19,
+        unitId: 4, // Unit 4 (Learn the basics...)
+        order: 4,
+        title: "Use the present tense",
+      },
+      {
+        id: 20,
+        unitId: 4, // Unit 4 (Learn the basics...)
+        order: 5,
+        title: "Get around town",
+      },
+      {
+        id: 21,
+        unitId: 5, // Unit 5 (Learn the basics...)
+        order: 1,
+        title: "Use basic phrases",
+      },
+      {
+        id: 22,
+        unitId: 5, // Unit 5 (Learn the basics...)
+        order: 2,
+        title: "Greet people",
+      },
+      {
+        id: 23,
+        unitId: 5, // Unit 5 (Learn the basics...)
+        order: 3,
+        title: "Introduce yourself",
+      },
+      {
+        id: 24,
+        unitId: 5, // Unit 5 (Learn the basics...)
+        order: 4,
+        title: "Use the present tense",
+      },
+      {
+        id: 25,
+        unitId: 5, // Unit 5 (Learn the basics...)
+        order: 5,
+        title: "Get around town",
+      },
+      {
+        id: 26,
+        unitId: 6, // Unit 6 (Learn the basics...)
+        order: 1,
+        title: "Use basic phrases",
+      },
+      {
+        id: 27,
+        unitId: 6, // Unit 6 (Learn the basics...)
+        order: 2,
+        title: "Greet people",
+      },
+      {
+        id: 28,
+        unitId: 6, // Unit 6 (Learn the basics...)
+        order: 3,
+        title: "Introduce yourself",
+      },
+      {
+        id: 29,
+        unitId: 6, // Unit 6 (Learn the basics...)
+        order: 4,
+        title: "Use the present tense",
+      },
+      {
+        id: 30,
+        unitId: 6, // Unit 6 (Learn the basics...)
+        order: 5,
+        title: "Get around town",
+      },
+      {
+        id: 31,
+        unitId: 7, // Unit 7 (Learn the basics...)
+        order: 1,
+        title: "Use basic phrases",
+      },
+      {
+        id: 32,
+        unitId: 7, // Unit 7 (Learn the basics...)
+        order: 2,
+        title: "Greet people",
+      },
+      {
+        id: 33,
+        unitId: 7, // Unit 7 (Learn the basics...)
+        order: 3,
+        title: "Introduce yourself",
+      },
+      {
+        id: 34,
+        unitId: 7, // Unit 7 (Learn the basics...)
+        order: 4,
+        title: "Use the present tense",
+      },
+      {
+        id: 35,
+        unitId: 7, // Unit 7 (Learn the basics...)
+        order: 5,
+        title: "Get around town",
+      },
     ]);
 
     await db.insert(schema.challenges).values([
@@ -119,6 +341,27 @@ const main = async () => {
       {
         id: 3,
         lessonId: 1, //basic phrases
+        type: "SELECT",
+        order: 3,
+        question: "Which one of these is “the man”?",
+      },
+      {
+        id: 4,
+        lessonId: 2, //basic phrases
+        type: "SELECT",
+        order: 1,
+        question: "Which one of these is “the cat”?",
+      },
+      {
+        id: 5,
+        lessonId: 2, //basic phrases
+        type: "SELECT",
+        order: 2,
+        question: "Which one of these is “one”?",
+      },
+      {
+        id: 6,
+        lessonId: 2, //basic phrases
         type: "SELECT",
         order: 3,
         question: "Which one of these is “the man”?",
@@ -193,6 +436,78 @@ const main = async () => {
       {
         id: 9,
         challengeId: 3,
+        imageSrc: "/boy.svg",
+        correct: false,
+        text: "le garçon",
+        audioSrc: "/fr_boy.mp3",
+      },
+      {
+        id: 10,
+        challengeId: 4, // Which one of these is "the cat"?
+        imageSrc: "/one.svg",
+        correct: false,
+        text: "un",
+        audioSrc: "/fr_one.mp3",
+      },
+      {
+        id: 11,
+        challengeId: 4,
+        imageSrc: "/cat.svg",
+        correct: true,
+        text: "le chat",
+        audioSrc: "/fr_cat.mp3",
+      },
+      {
+        id: 12,
+        challengeId: 4,
+        imageSrc: "/boy.svg",
+        correct: false,
+        text: "le garçon",
+        audioSrc: "/fr_boy.mp3",
+      },
+      {
+        id: 13,
+        challengeId: 5, // Which one of these is "the one"?
+        imageSrc: "/one.svg",
+        correct: true,
+        text: "un",
+        audioSrc: "/fr_one.mp3",
+      },
+      {
+        id: 14,
+        challengeId: 5,
+        imageSrc: "/cat.svg",
+        correct: false,
+        text: "le chat",
+        audioSrc: "/fr_cat.mp3",
+      },
+      {
+        id: 15,
+        challengeId: 5,
+        imageSrc: "/boy.svg",
+        correct: false,
+        text: "le garçon",
+        audioSrc: "/fr_boy.mp3",
+      },
+      {
+        id: 16,
+        challengeId: 6, // Which one of these is "the man"?
+        imageSrc: "/man.svg",
+        correct: true,
+        text: "l'homme",
+        audioSrc: "/fr_man.mp3",
+      },
+      {
+        id: 17,
+        challengeId: 6,
+        imageSrc: "/one.svg",
+        correct: false,
+        text: "un",
+        audioSrc: "/fr_one.mp3",
+      },
+      {
+        id: 18,
+        challengeId: 6,
         imageSrc: "/boy.svg",
         correct: false,
         text: "le garçon",
